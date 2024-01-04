0fed18f1eadc2aaa4559539a25195624685c084a
Navin Kumar <navintechwork@gmail.com>
2024-01-04 14:33:48 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,64 @@
+let data = [{"id":"6","subject":"ACCOUNTS","topic":"Invoicing","description":"Making invoices from 1) Delivery challan, 2) Measurements, 3) Complaints, 4) Directly","zhelp_id":"ZH-2023-12-11-58049","type":"VIDEO","doc":"https:\/\/youtu.be\/3qFoPpzqKhQ?feature=shared"},{"id":"20","subject":"ACCOUNTS","topic":"Invoicing for low side works","description":"Create accurate invoices based on all unbilled measuremenairtimets ","zhelp_id":"ZH-2023-12-20-58870","type":null,"doc":"https:\/\/youtu.be\/DvrMjtnN8so"},{"id":"21","subject":"ACCOUNTS","topic":"Invoicing for low side works","description":"Create accurate invoices based on all unbilled measurements ","zhelp_id":"ZH-2023-12-20-58873","type":null,"doc":"https:\/\/youtu.be\/DvrMjtnN8so"},{"id":"2","subject":"GENERAL","topic":"Opterix Vs Excel","description":"Why you should switch from Excel to Opterix Workflows","zhelp_id":"ZH-2023-12-11-58045","type":"VIDEO","doc":"https:\/\/youtu.be\/LLPJQgzK_Bc"},{"id":"3","subject":"GENERAL","topic":"Objectives of Opterix for HVAC","description":"3 challenges in HVAC projects","zhelp_id":"ZH-2023-12-11-58046","type":"VIDEO","doc":"https:\/\/youtu.be\/eAW2X84U3Nw?feature=shared"},{"id":"4","subject":"GENERAL","topic":"Department wise Overview","description":"Get your teams to work in close coordination","zhelp_id":"ZH-2023-12-11-58047","type":"VIDEO","doc":"https:\/\/youtu.be\/lXs72LZoWOY?feature=shared"},{"id":"7","subject":"GENERAL","topic":"Adopting Opterix within the first week","description":"3 things you can start doing on Opterix from the first week. 1) Attendance, 2) Handling complaints, 3) Managing inventory","zhelp_id":"ZH-2023-12-11-58050","type":"VIDEO","doc":"https:\/\/www.youtube.com\/watch?v=T6iuIJg8u3Y"},{"id":"9","subject":"GENERAL","topic":"Sales orders in one glance","description":"See in one glance, the complete overview of each order - order value, delivered value, invoiced amount, payment received, cost incurred, profit\/loss status.","zhelp_id":"ZH-2023-12-11-58052","type":"VIDEO","doc":"https:\/\/youtu.be\/xvIiELjk_8g?feature=shared"},{"id":"11","subject":"GENERAL","topic":"Attendance","description":"Sign in and sign out to register your attendance everyday","zhelp_id":"ZH-2023-12-11-58054","type":null,"doc":"https:\/\/youtu.be\/wuDMyeSi4Po?feature=shared"},{"id":"16","subject":"GENERAL","topic":"Attendance from your mobile","description":"See how to turn location on for Opterix for attendance. ","zhelp_id":"ZH-2023-12-17-58554","type":null,"doc":"https:\/\/youtu.be\/reR2fyMyYTA"},{"id":"23","subject":"GENERAL","topic":"First steps","description":"1. Exploring Dashboards 2. Adding new employees 3. Updating company detail 4. Our new help section","zhelp_id":"ZH-2023-12-22-59073","type":null,"doc":"https:\/\/youtu.be\/09NFIlkQ4Ds"},{"id":"17","subject":"PROJECTS","topic":"Material request","description":"Opterix will guide you to request correct items within the approved quantity. ","zhelp_id":"ZH-2023-12-17-58579","type":null,"doc":"https:\/\/youtu.be\/JyC19IFbWM0"},{"id":"19","subject":"PROJECTS","topic":"Measurement reporting ","description":"In your business, do you raise bills based on actual measurements of work delivered? Do your customers sometimes doubt the measurements that went into the invoices? No one will doubt your invoices if you raise them with Opterix.","zhelp_id":"ZH-2023-12-19-58756","type":null,"doc":"https:\/\/youtu.be\/NOsKGYnlpfI"},{"id":"12","subject":"PURCHASE","topic":"Price Check","description":"Protect your margins with effective planning from the start 1) Material planning, 2) Getting supplier offers, 3) Compare offer to your quoted rates to ensure profit margins are maintained.","zhelp_id":"ZH-2023-12-11-58126","type":null,"doc":"https:\/\/youtu.be\/3VdsEzdVrFk"},{"id":"25","subject":"PURCHASE","topic":"Simple Purchase Orders","description":"Creating a simple purchase order and tracking it. Use this method to create PO for ordering supplies, tools, etc. For projects, more suitable options are available.","zhelp_id":"ZH-2023-12-28-60311","type":null,"doc":"https:\/\/youtu.be\/0zRd0NIFLrY"},{"id":"1","subject":"SALES","topic":"Sales Overview","description":"Complete overview of sales process from enquiry to order","zhelp_id":"ZH-2023-12-11-58044","type":"VIDEO","doc":"https:\/\/youtu.be\/Xtd0GNHqq-Q"},{"id":"5","subject":"SALES","topic":"Retail Sales Overview","description":"Retail sales process from quotation to warranty","zhelp_id":"ZH-2023-12-11-58048","type":"VIDEO","doc":"https:\/\/youtu.be\/r2sxP3isXpY?feature=shared"},{"id":"15","subject":"SALES","topic":"Quick quotation","description":"Enquiry to quotation within minutes, right from your mobile","zhelp_id":"ZH-2023-12-16-58530","type":null,"doc":"https:\/\/youtu.be\/hD3_EzTQJOk"},{"id":"10","subject":"SERVICE","topic":"Service overview","description":"Seven reasons to pick Opterix for managing service effectively","zhelp_id":"ZH-2023-12-11-58053","type":"VIDEO","doc":"https:\/\/youtu.be\/W9jQcbOlNdw?feature=shared"},{"id":"14","subject":"SERVICE","topic":"Registering Service Requests","description":"Registering service requests is easy. Select customer, select site, note down the issue. In the next step select the machine that requires service. You can even add new machines to the system if the service request is from a new customer.","zhelp_id":"ZH-2023-12-14-58236","type":null,"doc":"https:\/\/youtu.be\/clljALWYKi0?feature=shared"},{"id":"13","subject":"SITE WORK","topic":"Handling Complaints","description":"Resolve complaints and generate service report from your phone.","zhelp_id":"ZH-2023-12-13-53680","type":null,"doc":"https:\/\/www.youtube.com\/watch?v=XVcCoABTIPU"},{"id":"8","subject":"STOCK","topic":"Inventory overview","description":"Delivery of returnable items. Delivering for projects. Purchase of out of stock items. Stock closing.","zhelp_id":"ZH-2023-12-11-58051","type":"VIDEO","doc":"https:\/\/www.youtube.com\/watch?v=unWWjVJFWcA"},{"id":"18","subject":"STOCK","topic":"Deliveries to projects","description":"Error free deliveries to projects against material request.","zhelp_id":"ZH-2023-12-18-58662","type":null,"doc":"https:\/\/youtu.be\/qwZGS7ghYQM"},{"id":"22","subject":"STOCK","topic":"Deliver to project. No material request","description":"Store is usually the first to get started with Opterix. Watch me present how the store can quickly make a delivery if the project bill of material has been created.","zhelp_id":"ZH-2023-12-22-59042","type":null,"doc":"https:\/\/youtu.be\/xDXhsck39ng"},{"id":"24","subject":"STOCK","topic":"Setting up inventory ","description":" To get started with Opterix for inventory management, the first step is to set the inventory levels for each item present in your warehouse. Once the initial levels of the stock have been set and you start using the system for receiving good and making deliveries, the stock levels are automatically adjusted to match the physical quantities of these items. See how the initial stock levels can be set easily on Opterix Workflows","zhelp_id":"ZH-2023-12-27-60296","type":null,"doc":"https:\/\/youtu.be\/ckbGNmXupCc"}];
+
+let dropdownOne = [... new Set(data.map(x=>x.subject))];
+let dropdownTwo = [... new Set(data.map(x=>x.topic))];
+let dropdownThree = [... new Set(data.map(x=>x.description))];
+
+console.log('dropdownOne--->',dropdownOne);
+console.log('dropdownTwo--->',dropdownTwo);
+console.log('dropdownOne--->',dropdownThree);
+
+let dropdownOneText = '';
+let dropdownTwoText = '';
+let dropdownThreeText = '';
+
+for(let i=0; i<dropdownOne.length; i++){
+    dropdownOneText += `<a class="dropdown-item" href="#">${dropdownOne[i]}</a>`;
+}
+
+for(let i=0; i<dropdownTwo.length; i++){
+    dropdownTwoText += `<a class="dropdown-item" href="#">${dropdownTwo[i]}</a>`;  
+}
+
+for(let i=0; i<dropdownThree.length; i++){
+    dropdownThreeText += `<a class="dropdown-item" href="#">${dropdownThree[i]}</a>`; 
+}
+
+document.getElementById("dropdownOne").innerHTML += dropdownOneText;
+document.getElementById("dropdownTwo").innerHTML += dropdownTwoText;
+document.getElementById("dropdownThree").innerHTML += dropdownThreeText;
+
+// var jsObjects = [
+//     {a: 1, b: 2}, 
+//     {a: 3, b: 4}, 
+//     {a: 5, b: 6},
+//     {a: 10, b: 6},
+//     {a: 7, b: 8}
+//  ];
+
+ function filterValue(obj, key, value) {
+    console.log(obj.filter(function(v){ return v[key] === value}));
+    return obj.filter(function(v){ return v[key] === value});
+  }
+
+
+let dataSubjectWise = [];
+
+for(let i=0; i<dropdownOne.length; i++){
+    dataSubjectWise.push(filterValue(data, "subject", dropdownOne[i]));
+}
+
+console.log('DataSubjectWise--->', dataSubjectWise);
+
+let bodyText = '';
+
+for(let i=0; i<dataSubjectWise.length; i++){
+    bodyText += `<div class="col-md-4" style="margin-top:40px"><h3>${dataSubjectWise[i][0].subject}</h3><div class="accordion" id="accordionExample">`
+    for(let j=0; j<dataSubjectWise[i].length; j++){
+        bodyText += `<div class="accordion-item"><h2 class="accordion-header" id="headingOne"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne${i}${j}" aria-expanded="false" aria-controls="collapseOne">${dataSubjectWise[i][j]['topic']}</button></h2><div id="collapseOne${i}${j}" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample"><div class="accordion-body">${dataSubjectWise[i][j].description}</div></div></div>`;
+    }
+    bodyText += '</div></div>'
+    console.log('DataSubjectWise line--->', dataSubjectWise);
+}
+
+document.getElementById("bodyText").innerHTML += bodyText;
